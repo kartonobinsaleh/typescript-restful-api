8eb6364cb0b7a444ae0b638c176647fc1eb676e0
Kartono666 <Kartono.saleh@gmail.com>
2024-06-20 22:43:44 +0700

    membuat service user login

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -2,12 +2,14 @@ import { prismaClient } from "../applications/database";
 import { ResponseError } from "../errors/response-error";
 import {
   CreateUserRequest,
+  LoginUserRequest,
   UserResponse,
   toUserResponse,
 } from "../models/user-model";
 import { UserValidation } from "../validations/user-validation";
 import { Validation } from "../validations/validation";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export class UserService {
   static async register(request: CreateUserRequest): Promise<UserResponse> {
@@ -33,5 +35,42 @@ export class UserService {
     });
 
     return toUserResponse(newUser);
+  }
+
+  static async login(request: LoginUserRequest): Promise<UserResponse> {
+    const loginRequest = Validation.validate(UserValidation.LOGIN, request);
+
+    let user = await prismaClient.user.findUnique({
+      where: {
+        username: loginRequest.username,
+      },
+    });
+
+    if (!user) {
+      throw new ResponseError(401, "Username or Password is wrong");
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginRequest.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      throw new ResponseError(401, "Username or Password is wrong");
+    }
+
+    user = await prismaClient.user.update({
+      where: {
+        username: loginRequest.username,
+      },
+      data: {
+        token: uuid(),
+      },
+    });
+
+    const response = toUserResponse(user);
+    response.token = user.token!;
+
+    return response;
   }
 }
