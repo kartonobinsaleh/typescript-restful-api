ef6b222c9a77e0f6ce37b2ac305b2021fe163ba6
Kartono666 <Kartono.saleh@gmail.com>
2024-06-22 15:24:31 +0700

    membuat user service update

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -4,6 +4,7 @@ import { ResponseError } from "../errors/response-error";
 import {
   CreateUserRequest,
   LoginUserRequest,
+  UpdateUserRequest,
   UserResponse,
   toUserResponse,
 } from "../models/user-model";
@@ -77,5 +78,29 @@ export class UserService {
 
   static async get(user: User): Promise<UserResponse> {
     return toUserResponse(user);
+  }
+
+  static async update(
+    user: User,
+    request: UpdateUserRequest
+  ): Promise<UserResponse> {
+    const updateRequest = Validation.validate(UserValidation.UPDATE, request);
+
+    if (updateRequest.name) {
+      user.name = updateRequest.name;
+    }
+
+    if (updateRequest.password) {
+      user.password = await bcrypt.hash(updateRequest.password, 10);
+    }
+
+    const result = await prismaClient.user.update({
+      where: {
+        username: user.username,
+      },
+      data: user,
+    });
+
+    return toUserResponse(result);
   }
 }
