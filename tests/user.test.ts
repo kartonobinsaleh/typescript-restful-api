ea79bac304dcc8daaddf0b93f9ccd6af6578d03e
Kartono666 <Kartono.saleh@gmail.com>
2024-06-22 16:13:29 +0700

    Fix testing API USER LOGOUT

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -176,3 +176,36 @@ describe("PATCH /api/users/current", () => {
     expect(await bcrypt.compare("update", user.password)).toBe(true);
   });
 });
+
+describe("DELETE /api/users/current", () => {
+  beforeEach(async () => {
+    await UserTest.create();
+  });
+
+  afterEach(async () => {
+    await UserTest.delete();
+  });
+
+  it("should be able to logout", async () => {
+    const response = await supertest(web)
+      .delete("/api/users/current")
+      .set("Authorization", "test");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data).toBe("OK");
+
+    const user = await UserTest.get();
+    expect(user.token).toBeNull();
+  });
+
+  it("should reject logout user if token is wrong", async () => {
+    const response = await supertest(web)
+      .delete("/api/users/current")
+      .set("Authorization", "salah");
+
+    logger.debug(response.body);
+    expect(response.status).toBe(401);
+    expect(response.body.errors).toBeDefined();
+  });
+});
