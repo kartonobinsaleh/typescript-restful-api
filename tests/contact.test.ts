a5212ba8d4c7a6e42357e306581b7b96c8cd8b80
Kartono666 <Kartono.saleh@gmail.com>
2024-06-23 20:49:27 +0700

    Fix Testing API UPDATE Contact

--- tests/contact.test.ts
+++ tests/contact.test.ts
@@ -103,3 +103,87 @@ describe("GET /api/contacts/:contactId", () => {
     expect(response.body.errors).toBeDefined();
   });
 });
+
+describe("PUT /api/contacts/:contactId", () => {
+  beforeEach(async () => {
+    await UserTest.create();
+    await ContactTest.create();
+  });
+
+  afterEach(async () => {
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
+
+  it("should be able update contact", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .put(`/api/contacts/${contact.id}`)
+      .set("Authorization", "test")
+      .send({
+        first_name: "update",
+        last_name: "update",
+        email: "update@gmail.com",
+        phone: "081333",
+      });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.id).toBeDefined();
+    expect(response.body.data.first_name).toBe("update");
+    expect(response.body.data.last_name).toBe("update");
+    expect(response.body.data.email).toBe("update@gmail.com");
+    expect(response.body.data.phone).toBe("081333");
+  });
+
+  it("should reject update contact if contactId is not found", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .put(`/api/contacts/${contact.id + 1}`)
+      .set("Authorization", "test")
+      .send({
+        first_name: "update",
+        last_name: "update",
+        email: "update@gmail.com",
+        phone: "081333",
+      });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(404);
+    expect(response.body.errors).toBeDefined();
+  });
+
+  it("should reject update contact if data is invalid", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .put(`/api/contacts/${contact.id}`)
+      .set("Authorization", "test")
+      .send({
+        first_name: "",
+        last_name: "",
+        email: "update",
+        phone: "",
+      });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toBeDefined();
+  });
+
+  it("should reject update contact if token is invalid", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .put(`/api/contacts/${contact.id}`)
+      .set("Authorization", "salah")
+      .send({
+        first_name: "update",
+        last_name: "update",
+        email: "update@gmail.com",
+        phone: "081333",
+      });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(401);
+    expect(response.body.errors).toBeDefined();
+  });
+});
