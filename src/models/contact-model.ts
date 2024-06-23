fc87654e4f0b4cb3779b6cbab811c4bb54755e2d
Kartono666 <Kartono.saleh@gmail.com>
2024-06-23 20:47:46 +0700

    menambahkan request update contact

--- src/models/contact-model.ts
+++ src/models/contact-model.ts
@@ -2,6 +2,13 @@ import { Contact } from "@prisma/client";
 
 // Request
 export type CreateContactRequest = {
+  first_name: string;
+  last_name?: string;
+  email?: string;
+  phone?: string;
+};
+
+export type UpdateContactRequest = {
   id: number;
   first_name: string;
   last_name?: string;
