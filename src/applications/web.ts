0e71df663cc2cdeaa5193eb201655714671cabaa
Kartono666 <Kartono.saleh@gmail.com>
2024-06-25 07:32:34 +0700

    Menambahkan config cors

--- src/applications/web.ts
+++ src/applications/web.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
 import { publicRouter } from "../routes/public-api";
 import { errorMiddleware } from "../middleware/error-middleware";
 import { apiRouter } from "../routes/api";
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 export const web = express();
 web.use(express.json());
+
+web.use(cors(corsOptions));
 
 web.use("/api", publicRouter);
 web.use("/api", apiRouter);
